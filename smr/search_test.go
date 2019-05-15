6eaf673e03305a39deaa6517d2b18e4097bb82e8
David Garfinkle <david@kn0x.io>
2019-05-14 23:32:23 -0400

    add search tests

---
+++ smr/search_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+	"fmt"
+	"io/ioutil"
+	"path"
+)
+
+func TestSearchPalestrina(t *testing.T) {
+	var err error
+	var pieceMap = make(map[uint32]CScore)
+	files, err := ioutil.ReadDir(path.Join(PALESTRINA, "pb_notes"))
+	xk(err)
+
+	query := UnmarshalNotesFromFile(path.Join(PALESTRINA, "double_leading_tone_query.pb_notes"))
+	for i, file := range files {
+		target := InitScoreFromFile(path.Join(PALESTRINA, "pb_notes", file.Name()))
+		pieceMap[(uint32)(i)] = target
+	}
+
+	occs := Search(pieceMap, query)
+
+	for i := range occs.Occurrences[:23] {
+		//fmt.Printf("%v: %v\n", files[occs.Occurrences[i].Pid], occs.Occurrences[i])
+	}
+}
+
+
