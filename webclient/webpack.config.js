45dd65eba214cb5ceaf484a6a13ac63f58d464af
David Garfinkle <david@kn0x.io>
2019-05-15 00:32:03 -0400

    You can set minimization with --mode production

--- webclient/webpack.config.js
+++ webclient/webpack.config.js
@@ -2,28 +2,25 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: __dirname + '/src/search.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'search.js'
-  },
-  optimization: {
-      minimize: false
-  },
-  module: {
+	entry: __dirname + '/src/search.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'search.js'
+	},
+	module: {
 		rules: [{
 			test: /\.css$/,
 			use: ['style-loader', 'css-loader']
 		}]
 	},
-  devServer: {
-    contentBase: __dirname,
-    compress: false,
-    port: 8080,
+	devServer: {
+		contentBase: __dirname,
+		compress: false,
+		port: 8080,
 		headers: {
 			'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*'
+			'Access-Control-Allow-Headers': '*'
 		}
 
-  }
+	}
 };
