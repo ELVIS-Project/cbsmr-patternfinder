77f7a38ba8670808441874b912b36c24167115b3
David Garfinkle <david@kn0x.io>
2019-06-27 05:15:12 -0400

    Generate test cases for helsinki-ttwi

--- tests/generate_test_cases.py
+++ tests/generate_test_cases.py
@@ -1,14 +1,59 @@
 import sys
 import os
 import csv
+import music21
 from indexer import client, indexers
 from proto import smr_pb2, smr_pb2_grpc
 
 CURDIR = os.path.abspath(os.path.dirname(__file__))
+PROJROOT = os.path.join(CURDIR, os.pardir)
+HELSINKI_TESTS = os.path.join(PROJROOT, "smr", "helsinki-ttwi", "tests")
 TESTDATA = os.path.join(CURDIR, "testdata")
 PALESTRINA = os.path.join(TESTDATA, "palestrina_masses")
 LEMSTROM = os.path.join(TESTDATA, "lemstrom2011")
 QUERIES = os.path.join(TESTDATA, "queries")
+
+def helsinki_ttwi():
+
+    for filename in ("leiermann.xml", "query_a.mid", "query_b.mid", "query_c.mid", "query_d.mid", "query_e.mid", "query_f.mid"):
+        filename_without_extension = os.path.splitext(os.path.basename(filename))[0]
+        with open(os.path.join(LEMSTROM, filename), "rb") as f:
+            data = f.read()
+            notes_df = indexers.notes(data)
+            vectors_df = indexers.intra_vectors(data, len(notes_df))
+            vectors_csv = indexers.intra_vectors_to_csv(vectors_df)
+
+        with open(os.path.join(HELSINKI_TESTS, filename_without_extension + ".csv"), "w", newline="") as csvfile:
+            csvfile.write(vectors_csv)
+
+    palestrina_occs = (
+        #("000000000011344_Missa-Primi-toni_Credo_Palestrina-Giovanni-Pierluigi-da_file2.mid", 1238, 1266),)
+        ("/Users/davidgarfinkle/elvis-project/patternfinder/music_files/corpus/Palestrina/Primi_toni_Credo_4.mid", 1238, 1266),)
+
+    for mass, start, end in palestrina_occs:
+        filename_without_extension = os.path.splitext(os.path.basename(mass))[0]
+        #with open(os.path.join(PALESTRINA, "mid", mass), "rb") as f:
+        with open(mass, "rb") as f:
+            data = f.read()
+            vectors_df = indexers.intra_vectors(data, len(notes_df))
+            vectors_df = vectors_df[(vectors_df['startIndex'] >= 1238) & (vectors_df['endIndex'] <= 1266)]
+            vectors_csv = indexers.intra_vectors_to_csv(vectors_df)
+
+        print(vectors_csv)
+        with open(os.path.join(HELSINKI_TESTS, filename_without_extension + ".csv"), "w", newline="") as csvfile:
+            csvfile.write(vectors_csv)
+
+    query = music21.stream.Score()
+    query.insert(0, music21.chord.Chord(['C4', 'E4', 'A4', 'C5']))
+    query.insert(1, music21.chord.Chord(['B-3', 'F4', 'B-4', 'D5']))
+    outpath = query.write('xml')
+    with open(outpath, "r") as f:
+        data = f.read()
+        double_leading_tone_query_notes = indexers.notes(data)
+        double_leading_tone_query_vectors = indexers.intra_vectors(data, len(double_leading_tone_query_notes))
+    with open(os.path.join(HELSINKI_TESTS, "double_leading_tone_query.csv"), "w", newline="") as csvfile:
+        csvfile.write(indexers.intra_vectors_to_csv(double_leading_tone_query_vectors))
+
 
 def palestrina():
     midi_dir = os.path.join(PALESTRINA, "mid")
@@ -23,38 +68,6 @@ def lemstrom():
         query = f"query_{q}"
         midi_file_path = os.path.join(LEMSTROM, query + ".mid")
         response = client.index_notes_write_response(midi_file_path, LEMSTROM)
-
-def lemstrom_csv():
-    # Vectors CSV for c library testing
-
-    for path, filenames in (
-        (LEMSTROM, ("leiermann.xml", "query_a.mid", "query_b.mid", "query_c.mid", "query_d.mid", "query_e.mid", "query_f.mid")),
-        (os.path.join(PALESTRINA, "mid"), ("000000000011521_Missa-Ut-re-mi-fa-sol-la_Credo_Palestrina-Giovanni-Pierluigi-da_file2.mid",))):
-
-        for filename in filenames:
-            print(filename)
-            filename_without_extension = os.path.splitext(os.path.basename(filename))[0]
-            with open(os.path.join(path, filename), "rb") as f:
-                data = f.read()
-                notes_df = indexers.notes(data)
-                vectors_df = indexers.intra_vectors(data, len(notes_df))
-                vectors_csv = indexers.intra_vectors_to_csv(vectors_df)
-
-            with open(os.path.join(path, filename_without_extension + ".csv"), "w", newline="") as csvfile:
-                csvfile.write(vectors_csv)
-
-"""
-    for filename in ("leiermann.xml", "query_a.mid", "query_b.mid", "query_c.mid", "query_d.mid", "query_e.mid", "query_f.mid"):
-        filename_without_extension = os.path.splitext(os.path.basename(filename))[0]
-        with open(os.path.join(LEMSTROM, filename), "rb") as f:
-            data = f.read()
-            notes_df = indexers.notes(data)
-            vectors_df = indexers.intra_vectors(data, len(notes_df))
-            vectors_csv = indexers.intra_vectors_to_csv(vectors_df)
-
-        with open(os.path.join(LEMSTROM, filename_without_extension + ".csv"), "w", newline="") as csvfile:
-            csvfile.write(vectors_csv)
-"""
 
 def other():
     client.index_notes_write_response(os.path.join(TESTDATA, "000000000000457_Castigans-castigavit_Josquin-Des-Prez_file3.xml"), TESTDATA)
@@ -83,7 +96,7 @@ def queries():
         f.write(double_leading_tone.SerializeToString())
 
 if __name__ == "__main__":
-    lemstrom_csv()
+    helsinki_ttwi()
     lemstrom()
     queries()
     other()
